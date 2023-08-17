e96db691223130781a817e9913d9b33c2cf42d53
kaitcham <chams4f@gmail.com>
2023-08-17 12:07:04 +0200

    Add project colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        dark: "#001342",
+        primary: "#001342",
+        secondary: "#8ACC50",
+        lightYellow: "#F4BA00",
+      },
       fontFamily: {
         montSerrat: ["Montserrat", "sans-serif"],
         raleWay: ["Raleway", "sans-serif"],
